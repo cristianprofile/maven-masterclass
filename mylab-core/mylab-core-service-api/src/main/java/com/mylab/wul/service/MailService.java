979f2bc253153569de8d3160d844b5ddb0cd9aa6
mariano <mariano@192.168.1.101>
2014-04-20 15:22:53 +0200

    comments interface methods

--- mylab-core/mylab-core-service-api/src/main/java/com/mylab/wul/service/MailService.java
+++ mylab-core/mylab-core-service-api/src/main/java/com/mylab/wul/service/MailService.java
@@ -4,6 +4,10 @@ import com.mylab.wul.dto.MailRequest;
 
 public interface MailService {
 
+    /**
+     * Method to send an email whith information request
+     * @param mailrequest mail request to send by email protocol
+     */
     public void sendMail(MailRequest mailrequest);
 
 }
