d96286ecefdebfe461d1547f8a2dfab40602414e
cristianprofile <cristianprofile@gmail.com>
2014-04-25 19:55:35 +0200

    added class and method comments

--- mylab-core/mylab-core-repository/src/main/java/com/mylab/wul/repository/BaseRepository.java
+++ mylab-core/mylab-core-repository/src/main/java/com/mylab/wul/repository/BaseRepository.java
@@ -7,7 +7,17 @@ import org.springframework.stereotype.Repository;
 
 import com.mylab.wul.domain.Base;
 
+/**
+ * Access to base domain entities
+ * @author cristian romero
+ *
+ */
 @Repository
 public interface BaseRepository extends JpaRepository<Base, Long> {
+    /**
+     * Find base entity by its name
+     * @param name value of base entity name
+     * @return base entity
+     */
     List<Base> findByName(String name);
 }
