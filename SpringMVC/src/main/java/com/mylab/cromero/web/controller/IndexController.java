7dc23ebe50785f60f3ee77b5e3f3a9778f0216d8
cristianprofile <cristianprofile@gmail.com>
2014-08-01 10:54:17 +0200

    deleted flex json, added jackson mapper and modify controller to be able to generate json

--- SpringMVC/src/main/java/com/mylab/cromero/web/controller/IndexController.java
+++ SpringMVC/src/main/java/com/mylab/cromero/web/controller/IndexController.java
@@ -6,8 +6,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
@@ -15,29 +13,28 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.mylab.cromero.dto.BaseRequest;
 import com.mylab.cromero.dto.BaseResponse;
 import com.mylab.cromero.service.BaseService;
 import com.mylab.cromero.web.form.BaseForm;
 
-import flexjson.JSONSerializer;
+
 
 @Controller
 @RequestMapping(value = "/")
 public class IndexController {
 
 	 private final Logger logger = LoggerFactory.getLogger(this.getClass());
-	
+
 	@Autowired
 	private BaseService baseService;
 
 
 	@RequestMapping( method = RequestMethod.GET)
 	public String printWelcome(ModelMap model) {
-	
-		
+
+
 		List<BaseResponse> findAllBases = baseService.findAllBases();
 		model.addAttribute("findAllBases", findAllBases);
 		return "hello";
@@ -46,36 +43,34 @@ public class IndexController {
 
 	@RequestMapping(value = "/rest", method = RequestMethod.GET)
 	@ResponseBody
-	public ResponseEntity<String> verIngredientes() {
+	public List<BaseResponse> verIngredientes() {
 		BaseRequest base = new BaseRequest();
 		base.setName("margarita");
 		baseService.saveBase(base);
 		base = new BaseRequest();
 		base.setName("masa-pan");
-		
+
 		baseService.saveBase(base);
 		List<BaseResponse> findAllBases = baseService.findAllBases();
-		JSONSerializer serializer = new JSONSerializer();
-		String serialize = serializer.exclude("*.class").serialize(findAllBases);
 		HttpHeaders headers = new HttpHeaders();
 		headers.add("Content-Type", "application/json; charset=utf-8"); 
-		return new ResponseEntity<String>(serialize, headers, HttpStatus.OK);
+		return findAllBases;
 
 	}
-	
-	
-	
-	
+
+
+
+
 	 @RequestMapping(value = "/addBase", method = RequestMethod.GET)
 	    public String addContact(ModelMap model) {
 	         model.addAttribute("base", new BaseForm());
 		 return "addBase";
-	         
-	    
+
+
 	    }
-	     
-	
-	
+
+
+
 	 @RequestMapping(value = "/addBase", method = RequestMethod.POST)
 	    public String addContact(@ModelAttribute("base")
 	    BaseForm base, BindingResult result,ModelMap model) {
@@ -87,8 +82,8 @@ public class IndexController {
 		 model.addAttribute("findAllBases", findAllBases);
 	     return "hello";
 	    }
-	     
 
-	
+
+
 
 }
