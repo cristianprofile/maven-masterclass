f3b1d3116816eb16e102131afc55d8aa2507ef1b
cristianprofile <cristianprofile@gmail.com>
2014-09-23 00:24:06 +0200

    deleted transaction annotation at service impl unit testing layer

---
+++ mylab-core/mylab-core-service-impl/src/test/java/com/mylab/cromero/service/BaseServiceImplTest.java
@@ -0,0 +1,69 @@
+package com.mylab.cromero.service;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.mylab.cromero.domain.Base;
+import com.mylab.cromero.dto.BaseRequest;
+import com.mylab.cromero.exception.BaseNotFoundException;
+import com.mylab.cromero.repository.BaseRepository;
+
+@RunWith(MockitoJUnitRunner.class)
+public class BaseServiceImplTest {
+
+	@InjectMocks
+	private BaseServiceImpl baseService;
+
+	@Mock
+	private BaseRepository baseRepository;
+
+    @Test(expected = BaseNotFoundException.class)
+    public void testDeleteBaseNotExist() {
+    	
+    	//create empty repository list to return when i call to findbyname method
+    	List<Base> listaBasesRepositorio=new ArrayList<Base>();
+    	when(this.baseRepository.findByName("margarita")).thenReturn(listaBasesRepositorio);
+        BaseRequest base = new BaseRequest();
+        base.setName("margarita");
+        baseService.deleteBase(base);
+       
+
+    }
+
+    @Test
+    public void testDeleteBaseOk() {
+    	List<Base> listaBasesRepositorio=new ArrayList<Base>();
+    	Base base2 = new Base();
+    	base2.setId(33L);
+    	base2.setName("margarita");
+    	listaBasesRepositorio.add(base2);
+    	when(this.baseRepository.findByName("margarita")).thenReturn(listaBasesRepositorio);
+        BaseRequest base = new BaseRequest();
+        base.setName("margarita");
+        baseService.saveBase(base);
+        //save must be called once time
+        verify(this.baseRepository, times(1)).save(any(Base.class));
+        baseService.deleteBase(base);
+        //delete must be called once time with base 2 id
+        verify(this.baseRepository, times(1)).delete(base2.getId());
+        
+        
+        
+    }
+	
+	
+	
+
+}
---
+++ mylab-core/mylab-core-service-impl/src/test/java/com/mylab/cromero/service/BaseServiceImplTestIT.java
@@ -0,0 +1,49 @@
+package com.mylab.cromero.service;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.mylab.cromero.dto.BaseRequest;
+import com.mylab.cromero.exception.BaseNotFoundException;
+import com.mylab.cromero.service.BaseService;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ActiveProfiles(profiles = "dev")
+@ContextConfiguration(locations = "classpath*:/META-INF/spring/applicationContext-service-test.xml")
+public class BaseServiceImplTestIT {
+
+    @Autowired
+    private BaseService baseService;
+
+    @Transactional
+    @Test(expected = BaseNotFoundException.class)
+    public void testDeleteBaseNotExist() {
+
+        BaseRequest base = new BaseRequest();
+        base.setName("margarita");
+        baseService.deleteBase(base);
+
+    }
+
+    @Transactional
+    @Test
+    public void testDeleteBaseOk() {
+
+        BaseRequest base = new BaseRequest();
+        base.setName("margarita");
+        baseService.saveBase(base);
+        Assert.assertEquals(baseService.findAllBases().size(), 1);
+        baseService.deleteBase(base);
+        Assert.assertEquals(baseService.findAllBases().size(), 0);
+        
+        
+        
+    }
+
+}
