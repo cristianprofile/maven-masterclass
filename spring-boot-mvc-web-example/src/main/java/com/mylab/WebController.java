24f13900e6be704b9a3b3bf05aa6793aa522f2df
cristianprofile <cristianprofile@gmail.com>
2015-02-15 10:31:37 -0500

    deleted rest controller

--- spring-boot-mvc-web-example/src/main/java/com/mylab/WebController.java
+++ spring-boot-mvc-web-example/src/main/java/com/mylab/WebController.java
@@ -3,13 +3,15 @@ package com.mylab;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.mylab.cromero.service.BaseService;
 
-@RestController
+@Controller
 @RequestMapping("/base")
 public class WebController {
 
@@ -27,8 +29,28 @@ public class WebController {
 	 */
 	@RequestMapping(method = RequestMethod.GET)
 	public String home() {
-		logger.info("home controller mapping");
+		logger.info("home controller mapping test");
 		return "home";
+	}
+	
+	/**
+	 * List of all users
+	 * 
+	 * @param model
+	 * @return
+	 */
+	@RequestMapping("/users")
+	public String users(Model model) {
+
+		logger.info("users");
+		return "users";
+	}
+	
+	
+	@ExceptionHandler(Exception.class)
+	public void error(Exception exception) {
+		exception.printStackTrace();
+		logger.info("users {}",exception);
 	}
 
 }
