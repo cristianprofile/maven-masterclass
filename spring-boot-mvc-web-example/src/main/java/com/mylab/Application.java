ecf624813350667dd6828badbd95aa7d8fbf7cc2
cristianprofile <cristianprofile@gmail.com>
2015-02-15 22:38:04 -0500

    added static content to web application

--- spring-boot-mvc-web-example/src/main/java/com/mylab/Application.java
+++ spring-boot-mvc-web-example/src/main/java/com/mylab/Application.java
@@ -71,11 +71,13 @@ public class Application {
 	public EmbeddedServletContainerCustomizer containerCustomizer() {
 	 
 	   return (container -> {
+		   
+		    ErrorPage error400Page = new ErrorPage(HttpStatus.BAD_REQUEST, "/400.html");
 	        ErrorPage error401Page = new ErrorPage(HttpStatus.UNAUTHORIZED, "/401.html");
 	        ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/404.html");
 	        ErrorPage error500Page = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/500.html");
 	 
-	        container.addErrorPages(error401Page, error404Page, error500Page);
+	        container.addErrorPages(error401Page, error404Page, error500Page,error400Page);
 	   });
 	}
 } 
